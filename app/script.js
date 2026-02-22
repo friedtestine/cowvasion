0e9e298a2b7735eddd8e254da5d3c494932e9702
friedtestine <goldenpathp2@gmail.com>
2026-02-22 08:22:06 +0800

    added clickable boxes

--- app/script.js
+++ app/script.js
@@ -8,18 +8,32 @@ function spawn() {
     update(object);
 }
 
+function onClick(object){
+    object.remove();
+    // added spawn call to maintain count.
+    spawn();
+}
+
 function update(object) {
+    // prevents crash; if object was clicked while animation was ending. 
+    if (!document.body.contains(object)) return;
+
+    console.log(document.querySelectorAll('.moving-box').length);
     const newPos = Math.floor(Math.random() * (90 - 5 + 1) + 5);
     object.style.top = `${newPos}vh`;
 
-    const duration = Math.random() * 10 + 2;
+    const duration = Math.random() * 8 + 2;
 
-    // resets animation
+    // resets animation.
     object.style.animation = 'none';
     object.offsetWidth;
     object.style.animation = `moveAcross ${duration}s linear`;
+
+    // 'click' takes too long.
+    object.addEventListener('mousedown', () => onClick(object));
 }
 
-for (let i = 0; i < 100; i++) {
+// sets amount of objects spawned. 
+for (let i = 0; i < 50; i++) {
     spawn();
 }
