8499c7217dd5edb6588c10287e3484ba0d40d207
friedtestine <goldenpathp2@gmail.com>
2026-02-22 08:02:07 +0800

    made dynamically generated boxes

--- app/script.js
+++ app/script.js
@@ -1,13 +1,25 @@
-const object = document.getElementById("moving-object");
+function spawn() {
+    let object = document.createElement('div');
+    object.classList.add('moving-box');
+    document.body.appendChild(object);
 
-function update() {
+    object.addEventListener('animationend', () => update(object));
+
+    update(object);
+}
+
+function update(object) {
     const newPos = Math.floor(Math.random() * (90 - 5 + 1) + 5);
     object.style.top = `${newPos}vh`;
+
+    const duration = Math.random() * 10 + 2;
 
     // resets animation
     object.style.animation = 'none';
     object.offsetWidth;
-    object.style.animation = 'moveAcross 5s linear';
+    object.style.animation = `moveAcross ${duration}s linear`;
 }
 
-object.addEventListener('animationend', update); 
+for (let i = 0; i < 100; i++) {
+    spawn();
+}
